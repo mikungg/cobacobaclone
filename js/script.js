26028b215639a04f46758593883dd4c1ad095e72
mikungg <mikhael.sappy@gmail.com>
2023-08-03 05:16:29 +0700

    Impelemented js to auto-slideshow

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const EMAIL = document.getElementById("email")
 const PACK_OPT = document.getElementById("pack")
 const SEND_BUTTON = document.getElementById("send")
 
-SEND_BUTTON.addEventListener("click", function(event){
+SEND_BUTTON.addEventListener("click", function(event) {
     event.preventDefault()
 
     if(CLIENTNAME.value == ""){
@@ -17,3 +17,33 @@ SEND_BUTTON.addEventListener("click", function(event){
         alert("Please choose a package that pick your interest")
     }
 })
+
+/* SLIDESHOW AUTO-SLIDE */
+const SLIDE = document.getElementsByClassName("slideshow")
+let slideindex = 1
+
+nextSlide(0)
+
+setInterval(function() {
+    console.log(slideindex)
+    nextSlide(1)
+}, 4000)
+
+function nextSlide(index) {
+    showSlide(slideindex += index)
+}
+
+function showSlide(index){
+    if(index > SLIDE.length) {
+        slideindex = 1
+    }
+    if(index < 1) {
+        slideindex = SLIDE.length
+    }
+
+    for (let i = 0; i < SLIDE.length; i++) {
+        SLIDE[i].style.display = "none"
+    }
+
+    SLIDE[slideindex-1].style.display = "flex"
+}
